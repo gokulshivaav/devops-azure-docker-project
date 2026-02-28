3981cff7b0ec0a8e8a5cc840a00d7c39d988ea17
gokulshivaav <gokulshivaav@gmail.com>
2026-02-28 16:18:14 +0000

    terraform fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,4 @@
 variable "ssh_public_key" {
+  description = "SHA256:O7L2ZfRzQvnLWyIs0gGMHEXzlzO/ActyCDeFLAjLOYw gokulshivaav@gmail.com"
   type        = string
-  description = "Paste your SSH public key here"
 }
